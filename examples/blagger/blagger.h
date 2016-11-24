8e3ddac0c7a8f3765a14006844ac3ebe4eef9295
Topland, Jostein <Jostein.Topland@nov.com>
2016-11-24 14:03:15 +0100

    init commit

---
+++ examples/blagger/blagger.h
@@ -0,0 +1,12 @@
+#ifndef BLAGGER_H
+#define BLAGGER_H
+
+#include "tinygamelib.h"
+
+class Blagger
+{
+public:
+    Blagger();
+};
+
+#endif
---
+++ examples/jumpinjackson/jumpinjackson.h
@@ -0,0 +1,12 @@
+#ifndef JUMPINJACKSON_H
+#define JUMPINJACKSON_H
+
+#include "tinygamelib.h"
+
+class Jumpinjackson
+{
+public:
+    Jumpinjackson();
+};
+
+#endif
