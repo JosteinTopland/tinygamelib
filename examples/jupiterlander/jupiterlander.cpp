728454a7114e20b113aa59a35ab89498a4c02edf
Jostein Topland <jtop>
2017-09-21 09:47:26 +0200

    Added JupiterLander as example

---
+++ examples/jupiterlander/jupiterlander.cpp
@@ -0,0 +1,13 @@
+#include "jupiterlander.h"
+
+JupiterLander::JupiterLander()
+{
+    //tgl.init(320, 200);
+    //TGL_Object obj = { "Brick", 0, 0, { 71, 8, 8, 1 }, false };
+    //tgl.mainLoop();
+}
+
+int main()
+{
+    JupiterLander();
+}
---
+++ examples/jupiterlander/jupiterlander.h
@@ -0,0 +1,12 @@
+#ifndef JUPITERLANDER_H
+#define JUPITERLANDER_H
+
+#include "tinygamelib.h"
+
+class JupiterLander
+{
+public:
+    JupiterLander();
+};
+
+#endif
